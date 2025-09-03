804e573102108a7032ca5f8298a5f92b91926ead
tschroeer72 <t.schroeer@web.de>
2025-09-03 12:34:47 +0200

    Vorlagenmenü eingebaut

--- data/navdata-verwaltung.ts
+++ data/navdata-verwaltung.ts
@@ -23,8 +23,71 @@
         title: "Email",
         href: "/verwaltung/email",
     },
+    // {
+    //     title: "Vorlagen",
+    //     href: "/verwaltung/vorlagen",
+    // },
+]
+
+type Subtitle = {
+    subtitle: string;
+    href: string;
+}
+
+type NavDataVordrucke = {
+    title: string;
+    subtitle: Subtitle[];
+}
+
+export const NavDataVordrucke: NavDataVordrucke[] = [
     {
-        title: "Vorlagen",
-        href: "/verwaltung/vorlagen",
+        title: "Spiele",
+        subtitle: [
+            {
+                subtitle: "6-Tage-Rennen",
+                href: "/vorlagen/spiele/6-Tage-Rennen.pdf"
+            },
+            {
+                subtitle: "Kombimeisterschaft",
+                href: "dummy"
+            },
+            {
+                subtitle: "Meisterschaft",
+                href: "dummy"
+            },
+            {
+                subtitle: "Blitztunier",
+                href: "dummy"
+            },
+            {
+                subtitle: "Weihnachtsbaum",
+                href: "/vorlagen/spiele/Weihnachtsbaum.pdf"
+            },
+        ]
+    },    {
+        title: "Mitglieder",
+        subtitle: [
+            {
+                subtitle: "Aktive Mitglieder",
+                href: "dummy"
+            },
+            {
+                subtitle: "Alle Mitglieder",
+                href: "dummy"
+            },
+        ]
+    },
+    {
+        title: "Kasse",
+        subtitle: [
+            {
+                subtitle: "Spielverluste",
+                href: "/vorlagen/kasse/Spielverluste.pdf"
+            },
+            {
+                subtitle: "Abrechnung",
+                href: "/vorlagen/kasse/Abrechnung.pdf"
+            },
+        ]
     },
 ]
