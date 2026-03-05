015e50c5688ef046f4348069fb136ef7ec5b90ee
tschroeer72 <t.schroeer@web.de>
2026-03-05 08:36:35 +0100

    Im PWA-Modus die Orientation auf auto gestellt

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,7 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#1e293b",
-        orientation: "portrait",
+        orientation: "any",
         icons: [
             {
                 src: "/icons/icon-192.png",
